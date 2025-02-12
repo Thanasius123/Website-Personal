3bda6c8d75e884c28d8622dc27ade04d7c8e3cf4
Thanasius123 <thansiustjoa@outlook.com>
2025-02-12 14:50:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const slideritems = document.querySelectorAll('.slider-items')
+
+let sliderActive = 1;
+
+if (slideritems){
+    slideritems.forEach((slider, index) => {
+        if (index === 0){
+            slider.setAttribute("data-show",  "show");
+        }else{
+            slider.setAttribute("data-show",  "hidden");
+        }
+    })
+
+    setInterval(() => {
+        slideritems.forEach((slider, index) => {
+            if(sliderActive === index) {
+                slider.setAttribute("data-show",  "show");
+            } else {
+                slider.setAttribute("data-show",  "hidden");
+            }
+        });
+
+        if(sliderActive === slideritems.length - 1) {
+            sliderActive = 0;
+        } else {
+            sliderActive++;
+        }
+
+    }, 5000)
+}
